48b13aa75253a5195f0ca715bec7a114e42f401a
NicholasTechmoverai <techmoversai@gmail.com>
2026-03-12 23:34:31 +0300

    http image url fix to https

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,4 +1,7 @@
-export const getMediaUrl = (item:any,mediaPath: string): string => {
-  if (!item.url) return mediaPath;
-  return `${item.url}${mediaPath}`;
+export const getMediaUrl = (item: any, mediaPath: string): string => {
+  if (!item?.url) return mediaPath;
+
+  const baseUrl = item.url.replace(/^http:\/\//i, "https://");
+
+  return `${baseUrl}${mediaPath}`;
 };
